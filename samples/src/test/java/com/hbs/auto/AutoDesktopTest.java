39ba49156e3f8ea90852202d17d74b2555ff84ff
zunzhuowei <1808862723@qq.com>
2022-05-10 19:11:53 +0800

    test file moves to the specified directory

--- samples/src/test/java/com/hbs/auto/AutoDesktopTest.java
+++ samples/src/test/java/com/hbs/auto/AutoDesktopTest.java
@@ -2,6 +2,7 @@ package com.hbs.auto;
 
 import com.hbs.auto.constants.ClickType;
 import com.hbs.auto.enties.MouseEvent;
+import com.hbs.auto.utils.OpenCvUtils;
 
 import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +16,7 @@ public class AutoDesktopTest {
         AutoRobot.create()
                 .keysGroup(KeyEvent.VK_WINDOWS, KeyEvent.VK_D)
                 .delay(100)
-                .click(ClickType.RIGHT, MouseEvent.of("20220509210444.png", 5000), 1);
+                .click(ClickType.RIGHT, MouseEvent.of(OpenCvUtils.filePath + "/testfiles/20220509210444.png", 5000), 1);
 
 //        for (int i = 0; i < 5; i++) {
 //            AutoRobot.create()
