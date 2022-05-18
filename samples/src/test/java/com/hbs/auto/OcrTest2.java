26740135ada12eaf5de15273e819fbb0990af41d
zunzhuowei <1808862723@qq.com>
2022-05-18 12:40:37 +0800

    test

--- samples/src/test/java/com/hbs/auto/OcrTest2.java
+++ samples/src/test/java/com/hbs/auto/OcrTest2.java
@@ -5,7 +5,6 @@ import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.leptonica.PIX;
 import org.bytedeco.opencv.opencv_java;
-import org.bytedeco.opencv.opencv_text.OCRTesseract;
 import org.bytedeco.tesseract.TessBaseAPI;
 
 import static org.bytedeco.leptonica.global.lept.pixDestroy;
@@ -15,7 +14,7 @@ import static org.bytedeco.leptonica.global.lept.pixRead;
  * 识别图片中的文字
  * Created by zun.wei on 2022/5/10.
  */
-public class OcrTest {
+public class OcrTest2 {
 
 
     public static void main(String[] args) {
@@ -25,22 +24,31 @@ public class OcrTest {
         TessBaseAPI api = new TessBaseAPI();
         // Initialize tesseract-ocr with English, without specifying tessdata path
         //if (api.Init("D:\\chrome Downloads", "eng") != 0) {
-        if (api.Init(OpenCvUtils.filePath + "/extralib/tess", "chi_sim+chi_tra+eng") != 0) {
+        if (api.Init(OpenCvUtils.filePath + "/extralib/tess", "chi_sim") != 0) {
             System.err.println("Could not initialize tesseract.");
             System.exit(1);
         }
 
         // Open input image with leptonica library
-        PIX image = pixRead(args.length > 0 ? args[0] : "testfiles/ch_ocr.png");
-        api.SetImage(image);
-        // Get OCR result
-        outText = api.GetUTF8Text();
-        System.out.println("OCR output:\n" + outText.getString());
+        String s = "image-%s.jpeg";
+        for (int i = 0; i < 687; i++) {
+            int idx = (i + 1);
+            String rep = idx < 10 ? "00" + idx :
+                    idx < 100 ? "0" + idx :
+                            idx < 1000 ? "" + idx : "idx";
+            String fileName = String.format(s, rep);
+            PIX image = pixRead("H:\\disk\\test\\" + fileName);
+            api.SetImage(image);
+            // Get OCR result
+            outText = api.GetUTF8Text();
+            System.out.println("OCR output:\n" + outText.getString());
 
-        // Destroy used object and release memory
+            // Destroy used object and release memory
+            outText.deallocate();
+            pixDestroy(image);
+        }
+
         api.End();
-        outText.deallocate();
-        pixDestroy(image);
     }
 
 }
